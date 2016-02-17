4aef4d6c65468fca5e6145b80f53f4fa2dfa49b4
chaseWilliams <bornofawesomeness@gmail.com>
2016-02-16 22:07:47 -0500

    new_post.rb now works

--- _tools/new_post.rb
+++ _tools/new_post.rb
@@ -1,28 +1,52 @@
 # new_post.rb
+
+# made a method for easier expansion of additional security against inconsistency
+# later down the line.
 def gatekeeper *values
     user_input = gets.chomp
     valid = false
-    values.each {|element| valid = user_input == values}
+    values.each {|element| valid = user_input == element}
     while !valid
         puts "I couldn't understand that, could you try that again?"
         user_input = gets.chomp
+        values.each {|element| valid = user_input == element}
     end
     user_input
 end
-        
+
 puts "Hello! This program is going to guide you through the creation of a
 new post for RHS Japan Club. Please keep in mind not to add
 any unnecessary quotation marks when using this software- just straightaway
 tell me what to use. First, could you tell the author name to use?"
-name = gets.chomp
+author = gets.chomp
 
-puts "Excellent, #{name}. Now, what layout will this blog post be in? Hint: 
+puts "Excellent, #{author}. Now, what layout will this blog post be in? Hint:
 It's likely in 'post' format."
 layout = gets.chomp
 
 puts "#{layout} layout it is! Now what shall I title this post?"
 title = gets.chomp
 
-puts "Shall I make the data current time as of right now (#{Time.now}), or do 
-    you want to have a different date? <now, new>"
-# add more!
+puts "Finally, what category is this under?"
+category = gets.chomp
+
+puts "Awesome! Right now, you currently are saying that the layout is #{layout},
+the title is \"#{title}\", the date will be #{Time.new.getlocal}, the author is
+#{author}, and the category is #{category}. If this is not correct, then please
+hit Control and C simultaneously, and then hit up arrow, and then enter to restart
+this program. Otherwise, type 'confirm' to create the file!"
+response = gatekeeper 'confirm'
+t = Time.new.getlocal
+Dir.chdir '../_posts'
+file = File.new "#{t.year}-#{t.month}-#{t.day}-#{title.gsub(' ', '-')}.markdown", 'w+'
+file.write "---
+layout: #{layout}
+title: \"#{title}\"
+date: #{t}
+authors: [#{author}]
+categories: #{category}
+---"
+file.close
+puts "Congrats, the post skeleton has been made! Please proceed to the _posts folders to view and edit your post, adding the actual contents of the said post.
+
+Once you finish the post, you should push the new post to your forked repository and make a pull request. Another Japan Club officer will review the post and make changes to it as necessary until it is ready to be pushed to the master repository."
